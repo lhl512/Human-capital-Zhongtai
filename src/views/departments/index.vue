5d16fe58c5fb3b18a313ccc2e122ec998e85dd9b
lhl0507 <1434272575@qq.com>
2021-05-07 09:20:52 +0800

    获取部门列表

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -20,6 +20,7 @@
 
 <script>
 import TreeTools from './components/tree-tools'
+import { getDepartments } from '@/api/departments'
 export default {
   components: {
     TreeTools
@@ -27,14 +28,23 @@ export default {
   data() {
     return {
       company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
-      departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
-        { name: '行政部', manager: '刘备' },
-        { name: '人事部', manager: '孙权' }],
+      departs: [],
       defaultProps: {
         label: 'name' // 表示 从这个属性显示内容
       }
     }
+  },
+  mounted() {
+    this.getDepartments()
+  },
+  methods: {
+    async getDepartments() {
+      const res = await getDepartments()
+      console.log(res)
+      this.departs = res.depts
+    }
   }
+
 }
 </script>
 <style scoped>
